67373c38ffbb761d0526b1214b2702e43c72023a
Nathan Krasney <natankrasney@gmail.com>
2020-03-02 15:35:27 +0200

    handle production

--- express-react-auth-simple/server/index.js
+++ express-react-auth-simple/server/index.js
@@ -1,9 +1,9 @@
 console.log("app is loading");
+
 const express = require("express");
 const app = express();
 const routeHelper = require("./routeHelper");
-
-
+const utils = require("./production_utils");
 
 // used for json inside body
 app.use(express.json());
@@ -16,6 +16,8 @@ app.post("/users/login", (req, res) => {
   routeHelper.login(req, res);
 });
 
+// --- must be last before listen because it handle non served route
+utils.handleProduction(express, app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
